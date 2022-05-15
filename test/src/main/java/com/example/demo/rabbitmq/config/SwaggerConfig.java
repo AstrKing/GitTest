f06da0fc78962306908b4fcd3924407b337cb4ce
root <myemail@qq.com>
2022-05-15 18:38:19 +0800

    1111111111111111

--- test/src/main/java/com/example/demo/rabbitmq/config/SwaggerConfig.java
+++ test/src/main/java/com/example/demo/rabbitmq/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.rabbitmq;
+package com.example.demo.rabbitmq.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
