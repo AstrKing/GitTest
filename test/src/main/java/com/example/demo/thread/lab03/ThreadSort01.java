5e77654908fdf7628903640a1f1d14f388540308
root <myemail@qq.com>
2022-05-25 10:56:09 +0800

    1111111111111111

---
+++ test/src/main/java/com/example/demo/thread/lab03/ThreadSort01.java
@@ -0,0 +1,21 @@
+package com.example.demo.thread.lab03;
+
+public class ThreadSort01 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            System.out.println("thread1");
+        });
+        Thread thread2 = new Thread(() -> {
+            System.out.println("thread2");
+        });
+        Thread thread3 = new Thread(() -> {
+            System.out.println("thread3");
+        });
+        thread1.start();
+        thread1.join();
+        thread2.start();
+        thread2.join();
+        thread3.start();
+        thread3.join();
+    }
+}
